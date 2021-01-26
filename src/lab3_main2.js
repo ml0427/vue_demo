ac8a8066c757fd20c0495665f8e7d0ddf14470ea
michaelNodeTest <ml0427@gmail.com>
2021-01-26 14:36:15 +0800

    vueDemo

---
+++ src/lab3_main2.js
@@ -0,0 +1,7 @@
+
+import course3 from "./lab3_course";
+import course4 from "./lab3_course.js";
+import * as bundle from  "./lab3_common";
+
+bundle.default_handler()
+console.log(`count max size = ${bundle.MAX_SIZE}`);
