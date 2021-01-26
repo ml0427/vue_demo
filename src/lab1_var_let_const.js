c9dc19cbec1c21f93559a30a2e5373b9446374e9
michaelNodeTest <ml0427@gmail.com>
2021-01-26 11:52:19 +0800

    add babel

---
+++ src/lab1_var_let_const.js
@@ -0,0 +1,13 @@
+var p = 5;
+console.log("p = ", p);
+let x = 5;
+console.log("x = ", x);
+
+p += 1;
+x += 2;
+console.log("p=", p, "x=", x);
+
+const y = "hello world x=${x}";
+console.log(y);
+const z = `hello world x=${x}`;
+console.log(z);
