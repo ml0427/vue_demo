f686ac7f9b03e55f2e022b0c68b66256a3cdfcf1
michaelNodeTest <ml0427@gmail.com>
2021-01-26 14:44:04 +0800

    vueDemo

---
+++ src/README.md
@@ -0,0 +1,6 @@
+# npm init -y
+# npm run build
+# execute import/export in plain node.js
+```
+node --es-module-specifier-resolution=node src\lab3_main.js
+```
--- src/lab3_main2.js
+++ src/lab3_main2.js
@@ -1,7 +1,7 @@
-
 import course3 from "./lab3_course";
 import course4 from "./lab3_course.js";
-import * as bundle from  "./lab3_common";
+import * as bundle from "./lab3_common";
 
-bundle.default_handler()
+bundle.default_handler();
+console.log(course3, course4);
 console.log(`count max size = ${bundle.MAX_SIZE}`);
